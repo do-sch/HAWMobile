3b8cd1631285144a5f54586c1af362fb8027564c
smaarti <martinbilda1@gmail.com>
2017-11-09 11:07:58 +0100

    ScheduleIcon in NavigationBar hinzugefügt; Package und ScheduleActivity erstellt;

---
+++ app/src/main/java/de/haw_landshut/hawmobile/schedule/ScheduleActivity.java
@@ -0,0 +1,14 @@
+package de.haw_landshut.hawmobile.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import de.haw_landshut.hawmobile.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
