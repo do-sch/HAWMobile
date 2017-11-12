cba8b9feedd2909652df4cd75711924963278355
SL315 <33317476+SL315@users.noreply.github.com>
2017-11-12 09:49:59 +0100

    Überarbeitung der MainActivity: BottomNavigationView hat jetzt Die richtigen Menüpunkte sowie Icons

--- app/src/main/java/de/haw_landshut/hawmobile/MainActivity.java
+++ app/src/main/java/de/haw_landshut/hawmobile/MainActivity.java
@@ -1,11 +1,14 @@
 package de.haw_landshut.hawmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import de.haw_landshut.hawmobile.mail.MailOverviewActivity;
+import de.haw_landshut.hawmobile.schedule.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +17,23 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        Intent in;
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.action_mail:
+                    in = new Intent(getBaseContext(), MailOverviewActivity.class);
+                    startActivity(in);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.action_schedule:
+                    in = new Intent(getBaseContext(), ScheduleActivity.class);
+                    startActivity(in);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.action_map:
+
                     return true;
-                case R.id.navigation_schedule:
-                    mTextMessage.setText(R.string.title_schedule);
+                case R.id.action_news:
+
                     return true;
             }
             return false;
