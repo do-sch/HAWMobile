c384dd5cb90476fcf140efcb2acb42e524796e17
SL315 <33317476+SL315@users.noreply.github.com>
2017-11-12 13:16:49 +0100

    Fragment Layout hinzugefügt

--- app/src/main/java/de/haw_landshut/hawmobile/mail/MailOverviewActivity.java
+++ app/src/main/java/de/haw_landshut/hawmobile/mail/MailOverviewActivity.java
@@ -2,9 +2,10 @@ package de.haw_landshut.hawmobile.mail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import de.haw_landshut.hawmobile.MainActivity;
 import de.haw_landshut.hawmobile.R;
 
-public class MailOverviewActivity extends AppCompatActivity {
+public class MailOverviewActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
