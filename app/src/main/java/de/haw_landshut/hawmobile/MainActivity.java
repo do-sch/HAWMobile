eb9ec843ec7fdf74193b1ea50d7342bbe7c0d4fe
SL315 <33317476+SL315@users.noreply.github.com>
2017-11-16 20:26:20 +0100

    Einbindung der LoginActivity

--- app/src/main/java/de/haw_landshut/hawmobile/MainActivity.java
+++ app/src/main/java/de/haw_landshut/hawmobile/MainActivity.java
@@ -1,13 +1,18 @@
 package de.haw_landshut.hawmobile;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 import de.haw_landshut.hawmobile.mail.MailOverviewActivity;
+import de.haw_landshut.hawmobile.mail.Protocol;
 import de.haw_landshut.hawmobile.schedule.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,6 +54,32 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        handleLogin();
+    }
+
+    private void handleLogin(){
+
+        //holt sich alle HAWMobile-Accounts auf dem Gerät
+        AccountManager accountManager = AccountManager.get(getApplicationContext());
+        Account[] accounts = accountManager.getAccountsByType("de.haw_landshut.hawmobile.ACCOUNT");
+        if (accounts.length == 0){
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        } else {
+
+            final Account a = accounts[0];
+            final String username = a.name;
+            final String password = accountManager.getPassword(a);
+            final Fakultaet fa = Fakultaet.get(accountManager.getUserData(a, "FACULTY"));
+
+            if (Protocol.testAccount(username, password))
+                Credentials.setCredentials(username, password, fa);
+            else {
+                Toast.makeText(getApplicationContext(), getString(R.string.login_failed), 3000);
+            }
+
+        }
+
     }
 
 }
