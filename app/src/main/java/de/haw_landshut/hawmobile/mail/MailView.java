8249f8f34b936bf02adeaeb5d00f1a650420a94a
SL315 <33317476+SL315@users.noreply.github.com>
2018-02-19 14:09:40 +0100

    Updated MailView

--- app/src/main/java/de/haw_landshut/hawmobile/mail/MailView.java
+++ app/src/main/java/de/haw_landshut/hawmobile/mail/MailView.java
@@ -2,20 +2,16 @@ package de.haw_landshut.hawmobile.mail;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
-import de.haw_landshut.hawmobile.MainActivity;
 import de.haw_landshut.hawmobile.R;
-import de.haw_landshut.hawmobile.base.EMail;
-import de.haw_landshut.hawmobile.base.EMailDao;
 
 import javax.mail.internet.ContentType;
 import javax.mail.internet.ParameterList;
@@ -23,7 +19,11 @@ import javax.mail.internet.ParseException;
 
 public class MailView extends AppCompatActivity {
 
+    public static final int REQUEST_CODE=1;
+    public static final String DELETE = "delete";
+
     private String subject, sender;
+    private int adapterPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +31,22 @@ public class MailView extends AppCompatActivity {
         setContentView(R.layout.activity_mail_view);
 
         final Intent intent = getIntent();
-        final String foldername = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_FNA);
         final String encoding = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_ENCODING);
         final String text = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_TEXT);
         subject = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_SUBJECT);
         sender = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_SENDER);
+        adapterPosition = intent.getIntExtra(MailEntryAdapter.ViewHolder.MESSAGE_ADAPTER_POSITION, -1);
 
+//        final String foldername = intent.getStringExtra(MailEntryAdapter.ViewHolder.MESSAGE_FNA);
 //        final EMail indicies = new EMail();
 //        indicies.setUid(uid);
 //        indicies.setFoldername(foldername);
 
         final ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(subject);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.setTitle(subject);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         final WebView wv = findViewById(R.id.mailWebView);
 
@@ -60,8 +63,6 @@ public class MailView extends AppCompatActivity {
         wv.loadDataWithBaseURL(null, text, contentType.getBaseType(), contentType.getParameter("encoding"), null);
 
         Log.d("encoding", encoding);
-
-//        new InsertEMail().execute(indicies);
 
     }
 
@@ -82,7 +83,11 @@ public class MailView extends AppCompatActivity {
         deleteItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-
+                final Intent i = new Intent();
+                i.putExtra(DELETE, true);
+                i.putExtra(MailEntryAdapter.ViewHolder.MESSAGE_ADAPTER_POSITION, adapterPosition);
+                setResult(RESULT_OK, i);
+                finish();
                 return true;
             }
         });
@@ -107,40 +112,31 @@ public class MailView extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private class InsertEMail extends AsyncTask<EMail, Void, EMail>{
-        @Override
-        protected void onPostExecute(EMail mail) {
-            final ActionBar actionBar = getSupportActionBar();
-            Log.d("mail", (mail==null)+"");
-            Log.d("subject", mail.getSubject());
-            actionBar.setTitle(mail.getSubject());
-            actionBar.setDisplayHomeAsUpEnabled(true);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-            final WebView wv = findViewById(R.id.mailWebView);
-
-            ContentType contentType;
-            try {
-                contentType = new ContentType(mail.getEncoding());
-            } catch (ParseException e) {
-                e.printStackTrace();
-                ParameterList pl = new ParameterList();
-                pl.set("encoding", "utf-8");
-                contentType = new ContentType("text", "html", pl);
-            }
-
-            wv.loadDataWithBaseURL(null, mail.getText(), contentType.getBaseType(), contentType.getParameter("encoding"), null);
-
-            Log.d("encoding", mail.getEncoding());
-        }
-
-        @Override
-        protected EMail doInBackground(EMail... eMail) {
-            final EMailDao dao = MainActivity.getHawDatabase().eMailDao();
-            Log.d("uid", eMail[0].getUid()+"");
-            Log.d("foldername", eMail[0].getFoldername());
-            final EMail mail = dao.getEmailFromUidAndFolder(eMail[0].getUid(), eMail[0].getFoldername());
-
-            return mail;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
+
+    /**
+     * Take care of popping the fragment back stack or finishing the activity
+     * as appropriate.
+     */
+    @Override
+    public void onBackPressed() {
+
+        final Intent i = new Intent();
+        i.putExtra(DELETE, false);
+        i.putExtra(MailEntryAdapter.ViewHolder.MESSAGE_ADAPTER_POSITION, adapterPosition);
+        setResult(RESULT_OK, i);
+        finish();
+
+    }
+
 }
