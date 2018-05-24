b4971ea3b6a720b08cf6f5e454f428eadeb17b8b
marco <marco.feder@gmx.de>
2018-05-24 15:48:03 +0200

    Bug fixes

--- app/src/main/java/de/haw_landshut/hawmobile/schedule/ScheduleFragment.java
+++ app/src/main/java/de/haw_landshut/hawmobile/schedule/ScheduleFragment.java
@@ -15,10 +15,7 @@ import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
 import com.flask.colorpicker.builder.ColorPickerClickListener;
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
-import de.haw_landshut.hawmobile.Credentials;
-import de.haw_landshut.hawmobile.Fakultaet;
-import de.haw_landshut.hawmobile.MainActivity;
-import de.haw_landshut.hawmobile.R;
+import de.haw_landshut.hawmobile.*;
 import de.haw_landshut.hawmobile.base.*;
 
 import java.text.DateFormat;
@@ -56,7 +53,6 @@ public class ScheduleFragment extends Fragment {
     private View bottomSheet;
     private List<CustomTimetable> timetable;
     protected ScheduleDao scheduleDao = MainActivity.getHawDatabase(getContext()).scheduleDao();
-    private LocationMarker locationmarker;
     private TextView[][] elements;
     private boolean isEven;
     private  String[] subjects;
@@ -267,7 +263,9 @@ public class ScheduleFragment extends Fragment {
         show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                locationmarker.showLocation(getEt_raum_text());
+               // final LocationMarker locationMarker=MapsFragment.newInstance();
+               // locationMarker.showLocation(getEt_raum_text());
+                //((MainActivity) getActivity()).changeFragment(locationMarker);
             }
         });
 
