6fc1527dc5214aef0e5c2f3c6941c7f034469d14
BBacim <33480115+BBacim@users.noreply.github.com>
2018-05-28 13:58:49 -0700

    Update NewsOverview.java

--- app/src/main/java/de/haw_landshut/hawmobile/news/NewsOverview.java
+++ app/src/main/java/de/haw_landshut/hawmobile/news/NewsOverview.java
@@ -9,9 +9,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
-//import android.app.Fragment;
+import android.app.Fragment;
 import android.os.Handler;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
